0c2f64db8b4a99abfcfcfc9c86f5255f056c81df
hwren <hwrenx@126.com>
2018-07-29 23:07:40 +0800

    Improve build script

--- build_linux.sh
+++ build_linux.sh
@@ -3,11 +3,38 @@
 #
 # Copyright (c) 2018~ Yiqun Xu
 #               2018~ Falei Luo
+#               2018~ Huiwen Ren
 #               2018~ VCL, NELVT, PKU
 
 ###############################
 # Sync repositories
 ###############################
+
+touch build.log
+echo "BUILD TIME: $(date +%Y-%m-%d)" > build.log
+
+if test -t 1 && which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        msg_color=$(tput setaf 3)$(tput bold)
+        error_color=$(tput setaf 1)$(tput bold)
+        reset_color=$(tput sgr0)
+    fi
+    ncols=$(tput cols)
+fi
+
+checkfail()
+{
+    echo "$error_color""ERROR: $@ failed.$reset_color"
+    exit 1
+}
+
+printLog()
+{
+    echo ">>> $@" >> $build_dir/build.log
+    $@ >> $build_dir/build.log
+}
+
 checkGitSources()
 {
     echo ">>>>"
@@ -17,8 +44,7 @@ checkGitSources()
     echo " -------------------------------------------------------------------------- "
     if [ ! -d $1 ]; then
         echo "$1 source not found, cloning"
-        git clone $2 $1
-        checkfail "$1 source: git clone failed"
+        git clone $2 $1  || checkfail "$1 source: git clone failed"
     else
         echo "$1 source found!"
     fi
@@ -47,35 +73,35 @@ checkGitSources davs2       https://github.com/pkuvcl/davs2.git
 ###############################
 # build xavs2 encoder
 ###############################
-echo "\033[33m start build xAVS2 encoder\033[0m"
+echo "$msg_color[Start building xAVS2 encoder]$reset_color"
 cd xavs2/build/linux  # xAVS2 directory
-./configure --prefix=$build_dir/avs2_lib \
+printLog ./configure --prefix=$build_dir/avs2_lib \
             --enable-pic \
             --enable-shared
-make -j8
-make install
-cd ../../../
+printLog make -j8 || checkfail "make failed"
+printLog make install
+cd -
 
 ###############################
 # build davs2 decoder
 ###############################
-echo "\033[33m start build dAVS2 decoder\033[0m"
+echo "$msg_color[Start building dAVS2 decoder]$reset_color"
 cd davs2/build/linux  # dAVS2 directory
-./configure --prefix=$build_dir/avs2_lib \
+printLog ./configure --prefix=$build_dir/avs2_lib \
             --enable-pic \
             --enable-shared
-make -j8
-make install
-cd ../../../
+printLog make -j8 || checkfail "make failed"
+printLog make install
+cd -
 
 ###############################
 # build ffmpeg decoder
 ###############################
-echo "\033[33m start build FFmpegAVS2\033[0m"
+echo "$msg_color[Start building FFmpegAVS2]$reset_color"
 cd FFmpegAVS2
 git checkout avs2
 export PKG_CONFIG_PATH=$build_dir/avs2_lib/lib/pkgconfig
-./configure \
+printLog ./configure \
   --prefix=$build_dir/avs2_lib \
   --enable-gpl \
   --enable-libxavs2 \
@@ -83,6 +109,9 @@ export PKG_CONFIG_PATH=$build_dir/avs2_lib/lib/pkgconfig
   --enable-shared \
   --enable-static
 
-make -j8
-make install
+printLog make -j8 || checkfail "make failed"
+printLog make install
 # tar -czvf ./ffmpeg_lib.tar.gz ./install
+cd -
+
+echo "Everything done!"
